6a55f49333a423e5881f5130d5b59e9554427ce6
Byron Omondi <omondibyron25@gmail.com>
2023-03-12 22:35:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+document.addEventListener("DOMContentLoaded", function() {
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "Your-Name is the champion";
+  document.body.appendChild(newHeader);
+});
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Your Name is the champion";
+document.body.appendChild(newHeader);
